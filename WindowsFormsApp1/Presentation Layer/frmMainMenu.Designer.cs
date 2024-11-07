a7f2051b01f3bc2ca16dbe6f2094b830510b3b5e
FrederikWKnoetze <frederikwknoetze@gmail.com>
2024-11-07 23:59:40 +0200

    added form add elements for 2 forms

---
+++ WindowsFormsApp1/Presentation Layer/frmMainMenu.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnViewStudents = new System.Windows.Forms.Button();
+            this.btnUpdateStudent = new System.Windows.Forms.Button();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnDeleteStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnViewStudents
+            // 
+            this.btnViewStudents.Location = new System.Drawing.Point(321, 109);
+            this.btnViewStudents.Name = "btnViewStudents";
+            this.btnViewStudents.Size = new System.Drawing.Size(132, 41);
+            this.btnViewStudents.TabIndex = 0;
+            this.btnViewStudents.Text = "View Students";
+            this.btnViewStudents.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateStudent
+            // 
+            this.btnUpdateStudent.Location = new System.Drawing.Point(321, 296);
+            this.btnUpdateStudent.Name = "btnUpdateStudent";
+            this.btnUpdateStudent.Size = new System.Drawing.Size(132, 41);
+            this.btnUpdateStudent.TabIndex = 2;
+            this.btnUpdateStudent.Text = "Update Student Information";
+            this.btnUpdateStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(321, 204);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(132, 41);
+            this.btnAddStudent.TabIndex = 3;
+            this.btnAddStudent.Text = "Add new Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteStudent
+            // 
+            this.btnDeleteStudent.Location = new System.Drawing.Point(321, 370);
+            this.btnDeleteStudent.Name = "btnDeleteStudent";
+            this.btnDeleteStudent.Size = new System.Drawing.Size(132, 41);
+            this.btnDeleteStudent.TabIndex = 4;
+            this.btnDeleteStudent.Text = "Delete Student";
+            this.btnDeleteStudent.UseVisualStyleBackColor = true;
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeleteStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.btnUpdateStudent);
+            this.Controls.Add(this.btnViewStudents);
+            this.Name = "frmMainMenu";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewStudents;
+        private System.Windows.Forms.Button btnUpdateStudent;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnDeleteStudent;
+    }
+}
+

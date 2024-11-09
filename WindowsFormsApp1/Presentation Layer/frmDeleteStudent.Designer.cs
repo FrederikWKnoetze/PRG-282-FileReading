b299bba86ec5227af87bde02b664c4039b40aebd
GideonVermeulen <63670713+GideonVermeulen@users.noreply.github.com>
2024-11-09 15:22:54 +0200

    Delete Student Completed

--- WindowsFormsApp1/Presentation Layer/frmDeleteStudent.Designer.cs
+++ WindowsFormsApp1/Presentation Layer/frmDeleteStudent.Designer.cs
@@ -62,6 +62,7 @@
             this.btnDelete.TabIndex = 2;
             this.btnDelete.Text = "Delete Selected";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // edtID
             // 
